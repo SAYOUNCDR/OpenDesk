289091df113a4b64fa9e7c2b5814b555d50244b7
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-04-25 22:45:13 +0530

    chore: add convex pre dev  table and integration with web-client f-monorepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,12 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math"
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/convex/_generated/api.js"
 
 export default function Page() {
+  const users = useQuery(api.users.getMany)
   return (
     <div className="flex min-h-svh p-6  justify-center items-center">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Hello App!</h1>
-          <Button className="mt-2">Button</Button>
-          <p className="mt-4">
-            The sum of 1 and 2 is: <strong>{add(1, 2)}</strong>
-          </p>
-        </div>
-        <div className="text-muted-foreground font-mono text-xs">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+      <p>Web App</p>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   )
 }
