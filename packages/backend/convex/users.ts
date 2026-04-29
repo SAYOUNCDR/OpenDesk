9dba0c263b37980c0286fd407329cf34db4ecbe8
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-04-29 21:32:25 +0530

    feat: implement user management queries and mutations with authenticated frontend UI integration

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,9 +1,26 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const getMany = query({
     args: {},
     handler: async (ctx) => {
         const users = await ctx.db.query("users").collect();
         return users;
+    },
+})
+
+
+export const add = mutation({
+    args: { },
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (identity === null) {
+            throw new Error("Not authenticated");
+        }
+        const userId = await ctx.db.insert("users", {
+            name:"Test user"
+        })
+
+        return userId;
     },
 })
