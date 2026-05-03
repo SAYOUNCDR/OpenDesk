7b485e10ae83e17328b1c76ec68a3c856ca6c640
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-05-03 22:52:21 +0530

    feat: add sign-in and sign-up pages, implement user addition in widget app, and enhance proxy middleware for route protection

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,7 @@
+const Test = () => {
+  return (
+    <div>This is the test page</div>
+  )
+}
+
+export default Test
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,21 @@
 "use client"
-import { useQuery } from "convex/react"
+import { useQuery, useMutation} from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.users.getMany)
+  const addUser = useMutation(api.users.add)
   return (
     <div className="flex flex-col min-h-svh p-6 justify-center items-center">
       <p>Widget App</p>
       {users?.map((user) => (
         <pre key={user._id}>{user.name}</pre>
       ))}
+
+      <Button onClick={() => addUser({})}>
+        Add user
+      </Button>
     </div>
   )
 }
